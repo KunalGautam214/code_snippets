fe6554d2a8b71c5337e7b015bd898d0d4ffbde8d
KunalGautam214 <kunalgautam214@gmail.com>
2024-08-03 13:38:47 +0530

    Sort mix items in python list

--- i_q_p_1/sort_mix_items.py
+++ i_q_p_1/sort_mix_items.py
@@ -1,3 +1,14 @@
 # Sort a mixed element list
 
+def mix_element_sort(each_item):
+    try:
+        item = int(each_item)
+        return (1, item)
+    except Exception:
+        return (0, each_item)
+
+
 mix_list = ["Sam", 10987, 2.0, "Hi", 39, "Hello12345", 56, 10.345678, "He!!0", 1]
+print('Original list', mix_list)
+mix_list.sort(key=mix_element_sort)
+print('Sorted list', mix_list)
